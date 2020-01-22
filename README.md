a998a0711d108ab1135b0363fcae4844eb536e06
Elison <elison.campos@gmail.com>
2020-01-22 17:36:42 -0300

    I was bored

--- README.md
+++ README.md
@@ -1,68 +1,28 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Stupid Impossible Game
 
-## Available Scripts
+## Instruções
 
-In the project directory, you can run:
+Clone o projeto e
 
-### `yarn start`
+```js
+yarn install
+yarn start
+```
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Como joga essa coisa?
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Até ter uma explicação dentro do rolê, é assim:
 
-### `yarn test`
+- Clica em qualquer quadrado
+- Os próximos quadrados disponiveis vão começar a piscar, clica no que quiser
+- Segue clicando até preencher tudo... ou dar game over.
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Qual a lógica dessa zica?
 
-### `yarn build`
+Num grid 10x10, você tem que preencher todos os quadrados de 1 até 100. Mas para isso, você
+deve seguir algumas regras:
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- Quando mover no sentido cardinal, pular duas casas
+- Quando mover no sentido intercardinal, pular uma casa.
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+E é isto.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,218 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from "react";
+import styled, { keyframes, createGlobalStyle } from "styled-components";
+import cx from "classnames";
 
-function App() {
+const GlobalStyles = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap");
+  body{
+    font-family: Permanent Marker;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #e1e1e1;
+`;
+
+const Tilesheet = styled.div`
+  background: white;
+`;
+
+const Row = styled.div`
+  display: flex;
+  border-top: 1px solid #999;
+  &:last-child {
+    border-bottom: 1px solid #999;
+  }
+`;
+
+const pulse = keyframes`
+  0% { background-color: white}
+
+  50% {
+    background-color: lightblue;
+  }
+
+  100%{ background-color: white;}
+`;
+
+const Square = styled.div`
+  width: 64px;
+  height: 64px;
+  border-right: 1px solid #999;
+  &:first-child {
+    border-left: 1px solid #999;
+  }
+
+  &.waiting {
+    cursor: pointer;
+  }
+
+  &.available {
+    animation: ${pulse} 0.8s linear infinite;
+    cursor: pointer;
+  }
+
+  span {
+    font-size: 32px;
+    line-height: 64px;
+    text-align: center;
+    display: block;
+    color: ${props => (props.active ? "#cc0000" : "#000000")};
+  }
+`;
+
+const GameOver = ({ resetFn }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Tilesheet>
+        <h1>Game Over</h1>
+        <button onClick={resetFn}>Começar denovo</button>
+      </Tilesheet>
+    </Wrapper>
   );
+};
+
+const Cell = ({
+  started,
+  children,
+  x,
+  y,
+  onClick,
+  curr,
+  chackAvailability
+}) => {
+  const isActive = chackAvailability(x, y);
+  const squareClasses = cx({
+    available: started && isActive && !children,
+    waiting: !started
+  });
+  return (
+    <Square
+      className={squareClasses}
+      active={parseInt(children) === curr}
+      onClick={() => {
+        if (!started || (isActive && !children)) {
+          onClick(x, y);
+        }
+      }}
+    >
+      <span>{children}</span>
+    </Square>
+  );
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const table = this.buildArray(10, 10);
+    this.initialState = {
+      started: false,
+      gameOver: false,
+      curr: 0,
+      table
+    };
+    this.state = this.initialState;
+  }
+
+  componentDidUpdate() {
+    const { started, gameOver } = this.state;
+    if (gameOver) return;
+    const available = document.querySelectorAll(".available");
+    if (started && !available.length) {
+      this.setState({ gameOver: true });
+    }
+  }
+
+  buildArray(w, h) {
+    const base = [];
+    for (let i = 0, max = w; i < max; i += 1) {
+      const a = new Array(h).fill(null);
+      base.push(a);
+    }
+    return base;
+  }
+
+  handlechackAvailability = (x, y) => {
+    const { lastX, lastY } = this.state;
+
+    // check for cardinal position
+    if (
+      ((x - 3 === lastX || x + 3 === lastX) && y === lastY) ||
+      ((y - 3 === lastY || y + 3 === lastY) && x === lastX)
+    ) {
+      return true;
+    }
+
+    // check for intercardinal position
+    if (
+      (x - 2 === lastX || x + 2 === lastX) &&
+      (y - 2 === lastY || y + 2 === lastY)
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  handleClick = (x, y) => {
+    const { table, curr, started } = this.state;
+    const newCurr = curr + 1;
+    table[x][y] = newCurr;
+    const newState = { table, curr: newCurr, lastX: x, lastY: y };
+    if (!started) {
+      newState.started = true;
+    }
+    this.setState(newState);
+  };
+
+  reset = () => {
+    const table = this.buildArray(10, 10);
+    const newState = { ...this.initialState, table };
+    this.setState(newState);
+  };
+
+  render() {
+    const { gameOver, table, curr, started } = this.state;
+    if (gameOver) {
+      return <GameOver resetFn={this.reset} />;
+    }
+
+    return (
+      <Fragment>
+        <GlobalStyles />
+        <Wrapper>
+          <Tilesheet>
+            {table.map((row, x) => {
+              return (
+                <Row key={`row-${x}`}>
+                  {row.map((cell, y) => {
+                    return (
+                      <Cell
+                        started={started}
+                        onClick={this.handleClick}
+                        chackAvailability={this.handlechackAvailability}
+                        key={`cell-${y}`}
+                        x={x}
+                        y={y}
+                        curr={curr}
+                      >
+                        {cell}
+                      </Cell>
+                    );
+                  })}
+                </Row>
+              );
+            })}
+          </Tilesheet>
+        </Wrapper>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
